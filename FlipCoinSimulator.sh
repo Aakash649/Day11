62880ad36182ce160413729891dd6cdb4cd88f18
Aakash Malvankar <10ajax649hamilton@gmail.com>
2020-07-19 17:08:09 +0530

    Simulator Count value

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,11 +1,27 @@
 #!/bin/bash -x
 
-x=$(($((RANDOM % 2))+1))
 
-if [ $x -eq 1 ]
-then
-	echo  "Head"
-else
-	echo "Tail"
-fi
+read -p "Enter the number of times to run the simulator: " n;
 
+
+h=0
+t=0
+y=0
+
+while [ $y -lt $n ]
+do
+	x=$(($((RANDOM % 2))+1))
+
+	if [ $x -eq 1 ]
+	then
+		echo  "Head"
+		h=$(($h+1))
+	else
+		echo "Tail"
+		t=$(($t+1))
+	fi
+	y=$(($y+1))
+done
+
+echo "Number of Head Count: " $h;
+echo "Number of Tail Count: " $t;
