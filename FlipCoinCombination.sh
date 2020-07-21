b8f4f534e028c945cbbdb3ed6a13089427b98c80
Aakash Malvankar <10ajax649hamilton@gmail.com>
2020-07-21 17:29:36 +0530

    Doublet

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -6,21 +6,34 @@ declare -A Dict
 for (( i=1; i<=100; i++ ))
 do
 x=$(($((RANDOM % 2))+1))
+q=$(($((RANDOM % 2))+1))
 
-	if [ $x -eq 1 ]
+	if [ $x -eq 1 ] && [ $q -eq 1 ]
 	then
-		echo "Heads"
+		echo "Double Heads"
 		y=$(($y+1))
+	elif [ $x -eq 1 ]&&[ $q -ne 1 ]
+	then
+		echo "Head Before Tail"
+		a=$(($a+1))
+	elif [ $x -ne 1 ]&&[ $q -eq 1 ]
+	then
+		echo "Tail Before Head"
+		b=$(($b+1))
 	else
-		echo "Tails"
+		echo "Double Tails"
 		z=$(($z+1))
 	fi
 done
 
 
-Dict[H]=$y
-Dict[T]=$z
+Dict[HH]=$y
+Dict[HT]=$a
+Dict[TH]=$b
+Dict[TT]=$z
 
 echo ${Dict[@]}
-echo "Head Percentage: " ${Dict[H]}
-echo "Tail Percentage: " ${Dict[T]}
+echo "Double Head Percentage: " ${Dict[HH]}
+echo "Head Before Tail Percentage: " ${Dict[HT]}
+echo "Tail Before Head Percentage: " ${Dict[TH]}
+echo "Double Tail Percentage: " ${Dict[TT]}
