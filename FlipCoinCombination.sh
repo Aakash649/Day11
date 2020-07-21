46ba5e2767a2083b1821cdfa59350837e058e644
Aakash Malvankar <10ajax649hamilton@gmail.com>
2020-07-21 18:16:57 +0530

    Triplet

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,39 +1,70 @@
 #!/bin/bash -x
-y=0
-z=0
+
 declare -A Dict
+a=0
+b=0
+c=0
+d=0
+e=0
+f=0
+g=0
+h=0
 
 for (( i=1; i<=100; i++ ))
 do
 x=$(($((RANDOM % 2))+1))
 q=$(($((RANDOM % 2))+1))
+r=$(($((RANDOM % 2))+1))
 
-	if [ $x -eq 1 ] && [ $q -eq 1 ]
+
+
+	if [ $x -eq 1 ] && [ $q -eq 1 ] && [ $r -eq 1 ]
 	then
-		echo "Double Heads"
-		y=$(($y+1))
-	elif [ $x -eq 1 ]&&[ $q -ne 1 ]
-	then
-		echo "Head Before Tail"
+		echo "H H H"
 		a=$(($a+1))
-	elif [ $x -ne 1 ]&&[ $q -eq 1 ]
+	elif [ $x -eq 1 ] && [ $q -eq 1 ] && [ $r -ne 1 ]
 	then
-		echo "Tail Before Head"
+		echo "H H T"
 		b=$(($b+1))
+	elif [ $x -eq 1 ]&&[ $q -ne 1 ]&&[ $r -eq 1 ]
+	then
+		echo " H T H"
+		c=$(($c+1))
+	elif [ $x -ne 1 ]&&[ $q -eq 1 ]&&[ $r -eq 1 ]
+	then
+		echo "T H H"
+		d=$(($d+1))
+	elif [ $x -ne 1 ]&&[ $q -ne 1 ]&&[ $r -eq 1 ]
+	then
+		echo "T T H"
+		e=$(($e+1))
+	elif [ $x -eq 1 ]&&[ $q -ne 1 ]&&[ $r -ne 1 ]
+	then
+		echo "T H T"
+		f=$(($f+1))
+	elif [ $x -eq 1 ]&&[ $q -ne 1 ]&&[ $r -ne 1 ]
+	then
+		echo "H T T"
+		g=$(($g+1))
 	else
-		echo "Double Tails"
-		z=$(($z+1))
+		echo "T T T"
+		h=$(($h+1))
 	fi
 done
 
 
-Dict[HH]=$y
-Dict[HT]=$a
-Dict[TH]=$b
-Dict[TT]=$z
+Dict[HHH]=$a
+Dict[HHT]=$b
+Dict[HTH]=$c
+Dict[THH]=$d
+Dict[TTH]=$e
+Dict[THT]=$f
+Dict[HTT]=$g
+Dict[TTT]=$h
 
-echo ${Dict[@]}
-echo "Double Head Percentage: " ${Dict[HH]}
-echo "Head Before Tail Percentage: " ${Dict[HT]}
-echo "Tail Before Head Percentage: " ${Dict[TH]}
-echo "Double Tail Percentage: " ${Dict[TT]}
+z=${#Dict[@]}
+
+for (( i=$HHH; i<=$z; i++ ))
+do
+	${!Dict[i]}"="${Dict[i]}
+done
