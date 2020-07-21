0cc3c730998f5c0a85198ab2a34316b9d1e809b8
Aakash Malvankar <10ajax649hamilton@gmail.com>
2020-07-21 17:15:15 +0530

    Singlet percentage

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,10 +1,26 @@
 #!/bin/bash -x
+y=0
+z=0
+declare -A Dict
 
+for (( i=1; i<=100; i++ ))
+do
 x=$(($((RANDOM % 2))+1))
 
-if [ $x -eq 1 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+	if [ $x -eq 1 ]
+	then
+		echo "Heads"
+		y=$(($y+1))
+	else
+		echo "Tails"
+		z=$(($z+1))
+	fi
+done
+
+
+Dict[H]=$y
+Dict[T]=$z
+
+echo ${Dict[@]}
+echo "Head Percentage: " ${Dict[H]}
+echo "Tail Percentage: " ${Dict[T]}
